d6a0fdf70c4786f720599a7aed4d763350a05318
meshramamrapali <amrapalimeshram86@gmail.com>
2023-03-05 23:00:44 +0530

    ADDED : Create Contacts in Address Book

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,6 +1,95 @@
 public class AddressBookMain {
-    public static void main(String[] args) {
+    public String firstname;
+    public String lastname;
+    public String address;
+    public String state;
+    public String city;
+    public int zip;
+    public int phone_number;
+    public String email;
 
-        System.out.println("Welcome To Address Book ");
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook1{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                ", state='" + state + '\'' +
+                ", phone_number=" + phone_number +
+                ", city='" + city + '\'' +
+                ", zip=" + zip +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        AddressBookMain addressBookMain = new AddressBookMain();
+        System.out.println(addressBookMain);
     }
 }
+
+
