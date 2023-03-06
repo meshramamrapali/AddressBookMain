08c58a23301c22fbed8b032ab261cc21219a7cce
meshramamrapali <amrapalimeshram86@gmail.com>
2023-03-06 13:25:34 +0530

    ADDED : Add A New Contact In Address Book

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,95 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookMain {
-    public String firstname;
-    public String lastname;
-    public String address;
-    public String state;
-    public String city;
-    public int zip;
-    public int phone_number;
-    public String email;
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public double getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(int phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "AddressBook1{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", address='" + address + '\'' +
-                ", state='" + state + '\'' +
-                ", phone_number=" + phone_number +
-                ", city='" + city + '\'' +
-                ", zip=" + zip +
-                ", email='" + email + '\'' +
-                '}';
-    }
+    ArrayList<Contact> arrayList = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    Contact[] contactsArray = new Contact[1];
 
     public static void main(String[] args) {
+        System.out.println("Welcome To Address Book");
         AddressBookMain addressBookMain = new AddressBookMain();
-        System.out.println(addressBookMain);
+        addressBookMain.addContact();
+    }
+
+    public void addContact() {
+        for (int i = 0; i < contactsArray.length; i++) {
+            Contact contact = new Contact();
+            System.out.println("Enter First Name :");
+            contact.firstname = scanner.next();
+            System.out.println("Enter Last Name :");
+            contact.lastname = scanner.next();
+            System.out.println("Enter Address :");
+            contact.address = scanner.next();
+            System.out.println("Enter State :");
+            contact.state = scanner.next();
+            System.out.println("Enter City :");
+            contact.city = scanner.next();
+            System.out.println("Enter Zip :");
+            contact.zip = scanner.nextInt();
+            System.out.println("Enter Phone_number :");
+            contact.phone_number = scanner.nextInt();
+            System.out.println("Enter Email :");
+            contact.email = scanner.next();
+            contactsArray[i] = contact;
+
+        }
     }
 }
+
 
 
